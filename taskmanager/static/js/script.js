8966494f049efa83b79cc9ea708e4c0ef4df46eb
Robn88 <robert.d.nicholas@gmail.com>
2022-03-04 14:40:48 +0000

    Create add task function

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(sidenav);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll(".datepicker");
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // select initialization
+    var selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
   });
+
+  
